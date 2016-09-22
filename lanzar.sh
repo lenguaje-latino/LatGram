e69753b1265ca0d19b9010f644f3ca5787257138
root <root@vps273405.ovh.net>
2016-09-22 12:02:02 +0200

    db

--- lanzar.sh
+++ lanzar.sh
@@ -15,6 +15,5 @@ while true; do {
 }
 
 if [ "$1" == true ]; then limpiar_updates;fi
-if [ "$1" == "limpiar" ]; then sudo tmux new-session -s "session" -d '. '$0' true';tmux attach -t "session"
-fi
+if [ "$1" == "limpiar" ]; then sudo tmux new-session -s "session" -d '. '$0' true';tmux attach -t "session";fi
 if [ ! $1 ]; then latino bot.lat;fi
