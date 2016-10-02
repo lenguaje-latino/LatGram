88b24fa4bd31bee9e22da4b81cd3466026667a15
Harry <jarriztg@users.noreply.github.com>
2016-10-01 21:18:12 -0500

    Update lanzar.sh

--- lanzar.sh
+++ lanzar.sh
@@ -1,3 +1,6 @@
+# Puedes cambiar el nombre de la sesión tmux si así deseas
+Sesion="Latino"
+
 function limpiar_updates() {
 
 while true; do {
@@ -15,6 +18,6 @@ while true; do {
 }
 
 if [ "$1" == true ]; then limpiar_updates;fi
-if [ "$1" == "limpiar" ]; then sudo tmux new-session -s "session" -d '. '$0' true';tmux attach -t "session";fi
-if [ ! $1 ]; then sudo tmux new-session -s "latino" -d 'latino bot.lat';tmux attach -t "latino";fi
-if [ "$1" == "matar" ]; then tmux kill-session -t "latino";fi
+if [ "$1" == "limpiar" ]; then sudo tmux new-session -s "temp" -d '. '$0' true';tmux attach -t "session";fi
+if [ ! $1 ]; then sudo tmux new-session -s $Sesion -d 'latino bot.lat';tmux attach -t $Sesion;fi
+if [ "$1" == "matar" ]; then tmux kill-session -t $Sesion;fi
